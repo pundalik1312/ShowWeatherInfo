fe7ff4a45d78fb4b8255f4d1a8434e7d07a9b113
pundalik <pundlik.sawant@gmail.com>
2023-01-03 12:19:46 -0600

    Add project files.

---
+++ Pages/ShowWeather.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ShowWeather.aspx.cs" Inherits="ShowWeatherInfo.Pages.ShowWeather" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Show Weather</title>
+    <style type="text/css">
+        .form-style-9 {
+            max-width: 450px;
+            background: #FAFAFA;
+            padding: 30px;
+            margin: 50px auto;
+            box-shadow: 1px 1px 25px rgba(0, 0, 0, 0.35);
+            border-radius: 10px;
+            border: 6px solid #305A72;
+        }
+    </style>
+</head>
+<body>
+
+ <form id="form1" runat="server" class="form-style-9">
+        <div>
+            <h1>Welcome to Weather Portal</h1>
+        </div>
+        <div>
+            <div>
+                <asp:TextBox runat="server" ID="txtCity" ></asp:TextBox> 
+                <asp:Button ID="showWeather" Text="Show Weather" runat="server" OnClick="showWeather_Click" />
+                <asp:Label ID="errLabel" runat="server" Visible="false" ></asp:Label>
+            </div>
+        </div>
+
+        <div>
+            <h2>Weather Info</h2>
+            <table>
+                <tr>
+                    <td>
+                        <strong>City: </strong>
+                        <asp:Label runat="server" ID="lblCity"></asp:Label>
+                        ,    
+                        <asp:Label runat="server" ID="lblCountry"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>Latitude: </strong>
+                        <asp:Label runat="server" ID="lblLat"></asp:Label>
+                        <br />
+                        <strong>Longitude: </strong>
+                        <asp:Label runat="server" ID="lblLon"></asp:Label>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>Description:</strong>
+                        <asp:Label runat="server" ID="lblDescription"></asp:Label>
+                        <br />
+                        <strong>Humidity:</strong>
+                        <asp:Label runat="server" ID="lblHumidity"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>Temperature (Feels Like) :</strong>
+                        <asp:Label runat="server" ID="lblTempFeelsLike"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>Temperature :</strong>
+                        <asp:Label runat="server" ID="lblTemp"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>Temperature (Min) :</strong>
+                        <asp:Label runat="server" ID="lblTempMin"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <strong>Temperature (Max) :</strong>
+                        <asp:Label runat="server" ID="lblTempMax"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Pages/ShowWeather.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Web.Script.Serialization;
+
+
+namespace ShowWeatherInfo.Pages
+{
+    public partial class ShowWeather : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void showWeather_Click(object sender, EventArgs e)
+        {
+            if (txtCity.Text != string.Empty)
+            {
+                errLabel.Visible = false;
+                errLabel.Text = "";
+                string City = txtCity.Text;
+                string appId = "384885c28ad209a656b006c2f7b8c1fa";
+                var url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", City, appId);
+                using (WebClient client = new WebClient())
+                {
+                    try
+                    {
+                        string json = client.DownloadString(url);
+
+                        //********************//  
+                        //     JSON RECIVED information   
+                        //********************//  
+                        //{"coord":{ "lon":72.85,"lat":19.01},  
+                        //"weather":[{"id":711,"main":"Smoke","description":"smoke","icon":"50d"}],  
+                        //"base":"stations",  
+                        //"main":{"temp":31.75,"feels_like":31.51,"temp_min":31,"temp_max":32.22,"pressure":1014,"humidity":43},  
+                        //"visibility":2500,  
+                        //"wind":{"speed":4.1,"deg":140},  
+                        //"clouds":{"all":0},  
+                        //"dt":1578730750,  
+                        //"sys":{"type":1,"id":9052,"country":"IN","sunrise":1578707041,"sunset":1578746875},  
+                        //"timezone":19800,  
+                        //"id":1275339,  
+                        //"name":"Mumbai",  
+                        //"cod":200}  
+
+                        //Converting to OBJECT from JSON string.  
+                        if (json != string.Empty)
+                        {
+                            RootObject weatherInfo = (new JavaScriptSerializer()).Deserialize<RootObject>(json);
+                            lblCountry.Text = weatherInfo.sys.country;
+                            lblCity.Text = weatherInfo.name;
+                            lblLat.Text = Convert.ToString(weatherInfo.coord.lat);
+                            lblLon.Text = Convert.ToString(weatherInfo.coord.lon);
+                            lblDescription.Text = weatherInfo.weather[0].description;
+                            lblHumidity.Text = Convert.ToString(weatherInfo.main.humidity);
+                            lblTemp.Text = Convert.ToString(weatherInfo.main.temp);
+                            lblTempFeelsLike.Text = Convert.ToString(weatherInfo.main.feels_like);
+                            lblTempMin.Text = Convert.ToString(weatherInfo.main.temp_max);
+                            lblTempMax.Text = Convert.ToString(weatherInfo.main.temp_min);
+                        }
+                        else
+                        {
+                            errLabel.Visible = true;
+                            errLabel.Text = "Please enter valid city";
+                            errLabel.ForeColor = Color.Red;
+                        }
+                    }
+                    catch
+                    {
+                        errLabel.Visible = true;
+                        errLabel.Text = "Please enter valid city";
+                        errLabel.ForeColor = Color.Red;
+                    }
+                }
+            }
+            else
+            {
+                errLabel.Visible = true;
+                errLabel.Text = "Please enter valid city";
+                errLabel.ForeColor = Color.Red;
+            }
+        }
+    }
+}
